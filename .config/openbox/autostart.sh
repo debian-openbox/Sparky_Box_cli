7d713ce19115193dcd189a8d7085ac9014bae473
debian-openbox <77241939+debian-openbox@users.noreply.github.com>
2022-01-03 19:20:34 +0100

    Update autostart.sh

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,28 +1,12 @@
 #! /bin/bash
 
 sparky-polkit &
+
 nitrogen --restore &
+
+export PATH="${PATH}:$HOME/.scripts"
+
 tint2 &
-
-xscreensaver -nosplash &
-(sleep 5; pnmixer) &
-if [ -f /opt/sparky/nm-applet-reload ]; then
-/opt/sparky/nm-applet-reload &
-fi
-if [ -f /usr/bin/xfce4-power-manager ]; then
-/usr/bin/xfce4-power-manager &
-fi
-if [ -f /usr/bin/xdg-user-dirs-update ]; then
-/usr/bin/xdg-user-dirs-update &
-fi
-
-sleep 10 && /usr/bin/conky-session &
-
-package-update-indicator &
-
-compton -cC -i 0.85 -e 0.85 -m 0.9 -r 4 -l -4 -t -4 &
-
-numlockx on &
 
 ~/keyboard.sh
 
@@ -30,4 +14,20 @@ fbxkb &
 
 urxvtd -q &
 
-export PATH="${PATH}:$HOME/.scripts"
+#~/conkystartup.sh
+sleep 10 && /usr/bin/conky-session &
+
+pnmixer &
+
+udiskie &
+
+package-update-indicator &
+
+compton -cC -i 0.85 -e 0.85 -m 0.9 -r 4 -l -4 -t -4 &
+# $HOME/bin/start-compton.sh &
+
+# Start screensaver
+xscreensaver -no-splash &
+
+# Turn on Numlock at start-up
+numlockx on &
